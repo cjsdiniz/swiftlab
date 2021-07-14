1e9f05a74b75ef465fa7d1e43f54dfbb719c3e9c
cjdiniz <cjsdiniz@outlook.com>
2021-07-14 19:39:09 +0100

    SDxApp

---
+++ SDxApp/SDxApp/SDxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDxAppApp.swift
+//  SDxApp
+//
+//  Created by Carlos Diniz on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SDxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
